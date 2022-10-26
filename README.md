4c8929112578e6524ecac99e3548a8ed472b63a0
bharathishadow <113233134+bharathishadow@users.noreply.github.com>
2022-10-26 19:54:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todo-list
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-12g8ph)
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+import "./style.css";
+import { AiFillDelete } from 'react-icons/ai';
+
+
+export default function App() {
+  const[todoList, setTodoList] = useState([])
+  const[newTask, setNewTask] = useState("")
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const addTask = () => {
+    const task = {
+      id:todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask ,
+    }
+    setTodoList([...todoList,task])
+  }
+
+  const deleteTask = (id) => {
+    const newTodoList = todoList.filter((task) => {
+      return task.id !== id
+      // if (task === taskName){
+      //   return false
+      // }else {
+        
+      //   return true
+      // }
+    })
+    setTodoList(newTodoList)
+  }
+
+  return (
+    <div>
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add items</button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+         return <div className="l-list">
+            <p>{task.taskName}</p>
+            <AiFillDelete onClick={() => deleteTask(task.id)}  />
+            
+         </div>
+        })}
+      </div>
+      
+    </div>
+  );
+}
